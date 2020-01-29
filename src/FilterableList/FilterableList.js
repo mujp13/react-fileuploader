521197f5c5edd2b31947d45e3d9e927607f6ed76
Kevin Park <keunsuk413@gmail.com>
2020-01-28 20:11:53 -0600

    react cp12 commit

---
+++ src/FilterableList/FilterableList.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './FilterableList.css';
+import ListItem from '../ListItem/ListItem';
+
+class FilterableList extends Component {
+  render() {
+    const { searchTerm, filterOption } = this.props;
+    const list = this.props.files
+        .filter(file => file.name.includes(searchTerm)
+              && (filterOption === 'All' || file.status === filterOption))
+        .map((file, key) => <ListItem {...file} key={key} />);
+    
+    /*const list = this.props.files 
+      ? this.props.files.map((file, key) => <ListItem {...file} key={key} />)
+      : null;*/ 
+    return (
+      <div className="FilterableList">
+        {list}
+      </div>
+    );
+  }
+}
+
+export default FilterableList;
